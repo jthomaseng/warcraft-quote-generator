1763d69d156464cee5fe4188fa4b05b825bd8c12
Jason Thomas <jason.thomas.eng@gmail.com>
2023-09-07 11:35:11 -0700

    Add Orc Grunt and Raider

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Mixed Messages
+# Warcraft Quote Generator
+
+## Overview
+This project consists of a single script that will return a random Warcraft III orc quote.
+
+
+
+
+All quotes pulled from: https://wowwiki-archive.fandom.com/wiki/Quotes_of_Warcraft_III/Orc_Horde#Orc_Grunt
--- randomOrc.js
+++ randomOrc.js
@@ -1,13 +1,65 @@
-const orcPeonQuotes = ['Ready to Work', 'Yes?', 'Hmmm?', 'What you want?',
+
+//Select a random quote from a speaker object and
+//add quotation marks and attribution.
+const randomQuote = (obj) => {
+    let i = Math.floor(Math.random() * obj.quoteList.length);
+    let quote = obj.quoteList[i];
+    let finalQuote = "\"" + quote + '\" - ' + obj.speaker;
+    return finalQuote;
+}
+
+//Create a new speaker object
+const speakerFactory = (speaker, quoteList) => {
+    let newSpeaker = {
+        speaker,
+        quoteList
+    }
+    return newSpeaker;
+}
+
+//Select a random speaker and return a quote
+const randomMessage = (speakerList) => {
+    let i = Math.floor(Math.random() * speakerList.length);
+    return randomQuote(speakerList[i]);
+}
+
+//Create a master list of speakers: 
+//Don't forget to add to this as you create new speakers
+let speakerMasterList = [];
+
+//CREATE OUR ORCS HERE
+//----------------------
+//  1) Create Quote List
+//  2) Create Speaker
+//  3) Add Speaker to Master
+
+//Create an Orc Peon: 
+let orcPeonQuotes = ['Ready to Work', 'Yes?', 'Hmmm?', 'What you want?',
 'Something need doing?', 'I can do that.', 'Be happy to.', 'Work, work.',
 'Okie dokie.', 'OK!', "Kill 'em", "I'll try...", "WHy not?!", 'Whaaat?',
 'Me busy. Leave me alone!!', 'No time for play.', 'Me not that kind of orc!'];
+let orcPeon = speakerFactory("Orc Peon", orcPeonQuotes);
+speakerMasterList.push(orcPeon);
 
-const randomMessage = (messageArray) => {
-    let number = Math.floor(Math.random() * messageArray.length);
-    let message = messageArray[number];
-    console.log(number);
-    console.log(message);
-}
+//Create an Orc Grunt: 
+let orcGruntQuotes = ['My life for the Horde!', 'Yes?', 'Huh?', 'Master?',
+'What you want?', 'Dabu!', "Lok'tar!", "Swobu!", "Zug zug!", "Time for killing!",
+"Hragh!", "Time to die!", "For the Horde!", "Why you poking me again?",
+"Why don't you lead an army instead of touching me?!", "Poke, poke, poke - is that all you do?",
+"Oh, that was kind of nice.", "Me no sound like Yoda. Do I?",
+"It not easy being green!"];
+let orcGrunt = speakerFactory("Orc Grunt", orcGruntQuotes);
+speakerMasterList.push(orcGrunt);
 
-randomMessage(orcPeonQuotes);
+//Create an Orc Raider:
+let orcRaiderQuotes = ["Ready to ride!", "Yes, chieftain?", "Hmm?",
+"Need something?", "Say the word.", "No problem.", "Yeea!", "Mush!",
+"Let's ride.", "Take 'em", "Taste my steel!", "For Doomhammer!",
+"For the glory of the Warchief!", "Down, boy.", "You're annoying my dog.",
+"What's that smell? Oh, bad dog!", "I'm hungry like the wolf!"];
+let orcRaider = speakerFactory("Orc Raider", orcRaiderQuotes);
+speakerMasterList.push(orcRaider);
+
+
+//Print a random quote to the console
+console.log(randomMessage(speakerMasterList));
